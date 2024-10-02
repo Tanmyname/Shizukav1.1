3f352b8ca20cd10dedb558d02628e7891f8d0006
Tanmyname <tanxzxzxzxz@gmail.com>
2024-10-02 23:02:52 +0800

    Add files via upload

--- Szhizuka.js
+++ Szhizuka.js
@@ -63,6 +63,7 @@ const { isSetProses, addSetProses, removeSetProses, changeSetProses, getTextSetP
 const { addResponList, delResponList, isAlreadyResponList, isAlreadyResponListGroup, sendResponList, updateResponList, getDataResponList } = require('./lib/respon-list');
 const { isSetDone, addSetDone, removeSetDone, changeSetDone, getTextSetDone } = require('./lib/setdone');
 let autosticker = JSON.parse(fs.readFileSync('./database/autosticker.json'))
+let ntnsfw = JSON.parse(fs.readFileSync('./database/nsfw.json'))
 let mute = JSON.parse(fs.readFileSync('./database/mute.json'));
 let ntvirtex = JSON.parse(fs.readFileSync('./database/antivirus.json'))
 let _cmd = JSON.parse(fs.readFileSync('./database/command.json'));
@@ -30198,67 +30199,72 @@ case 'xnxxsearch': {
               if (res.status) replyTan(ff)
               }
               break
-              case 'pin':
-              case 'pinterest': {
-    let input = "> _contoh: pin Marsha lenathea_";
-    if (!text) return reply(input);
-     reply('*Loading....*');
-    const createImage = async (url) => {
-        const { imageMessage } = await baileys.generateWAMessageContent({
-            image: {
-                url
-            }
-        }, {
-            upload: Tan.waUploadToServer
-        });
-        return imageMessage;
-    };
-    async function pinterest(query) {
-        let res = await fetch(`https://www.pinterest.com/resource/BaseSearchResource/get/?source_url=%2Fsearch%2Fpins%2F%3Fq%3D${query}&data=%7B%22options%22%3A%7B%22isPrefetch%22%3Afalse%2C%22query%22%3A%22${query}%22%2C%22scope%22%3A%22pins%22%2C%22no_fetch_context_on_resource%22%3Afalse%7D%2C%22context%22%3A%7B%7D%7D&_=1619980301559`);
-        let json = await res.json();
-        let data = json.resource_response.data.results;
-        if (!data.length) reply(`Query "${query}" not found :/`);
-        return data[~~(Math.random() * data.length)].images.orig.url;
-    }
-    const imageUrls = [];
-    for (let i = 0; i < 10; i++) {
-        const imageUrl = await pinterest(text);
-        imageUrls.push(imageUrl);
-    }
-    const cards = await Promise.all(imageUrls.map(async (url, index) => ({
-        header: proto.Message.InteractiveMessage.Header.fromObject({
-            title: `Image ${index + 1}`,
-            hasMediaAttachment: true,
-            imageMessage: await createImage(url)
-        }),
-        nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({
-            buttons: [] // Hapus semua tombol
-        })
-    })));
-    const msg = baileys.generateWAMessageFromContent(m.chat, {
-        viewOnceMessage: {
-            message: {
-                messageContextInfo: {
-                    deviceListMetadata: {},
-                    deviceListMetadataVersion: 2
-                },
-                interactiveMessage: proto.Message.InteractiveMessage.fromObject({
-                    body: proto.Message.InteractiveMessage.Body.fromObject({
-                        text: `${text}\n> Batas 10 photo`
-                    }),
-                    carouselMessage: proto.Message.InteractiveMessage.CarouselMessage.fromObject({
-                        cards
-                    })
-                })
-            }
-        }
-    }, { quoted: fcall });
-
-    await Tan.relayMessage(msg.key.remoteJid, msg.message, {
-        messageId: msg.key.id
-    });
-};
+              
+case "pin":
+let pn =`Fitur ini membutuhkan apikey buka file Shizuka.js dan masukkan apikey mu di case 'pin'`
+reply(pn)
 break
+              // case 'pin':
+//               case 'pinterest': {
+//     let input = "> _contoh: pin Marsha lenathea_";
+//     if (!text) return reply(input);
+//      reply('*Loading....*');
+//     const createImage = async (url) => {
+//         const { imageMessage } = await baileys.generateWAMessageContent({
+//             image: {
+//                 url
+//             }
+//         }, {
+//             upload: Tan.waUploadToServer
+//         });
+//         return imageMessage;
+//     };
+//     async function pinterest(query) {
+//         let res = await fetch(`Masukkan apikey mu di sini`);
+//         let json = await res.json();
+//         let data = json.resource_response.data.results;
+//         if (!data.length) reply(`Query "${query}" not found :/`);
+//         return data[~~(Math.random() * data.length)].images.orig.url;
+//     }
+//     const imageUrls = [];
+//     for (let i = 0; i < 10; i++) {
+//         const imageUrl = await pinterest(text);
+//         imageUrls.push(imageUrl);
+//     }
+//     const cards = await Promise.all(imageUrls.map(async (url, index) => ({
+//         header: proto.Message.InteractiveMessage.Header.fromObject({
+//             title: `Image ${index + 1}`,
+//             hasMediaAttachment: true,
+//             imageMessage: await createImage(url)
+//         }),
+//         nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({
+//             buttons: [] // Hapus semua tombol
+//         })
+//     })));
+//     const msg = baileys.generateWAMessageFromContent(m.chat, {
+//         viewOnceMessage: {
+//             message: {
+//                 messageContextInfo: {
+//                     deviceListMetadata: {},
+//                     deviceListMetadataVersion: 2
+//                 },
+//                 interactiveMessage: proto.Message.InteractiveMessage.fromObject({
+//                     body: proto.Message.InteractiveMessage.Body.fromObject({
+//                         text: `${text}\n> Batas 10 photo`
+//                     }),
+//                     carouselMessage: proto.Message.InteractiveMessage.CarouselMessage.fromObject({
+//                         cards
+//                     })
+//                 })
+//             }
+//         }
+//     }, { quoted: fcall });
+// 
+//     await Tan.relayMessage(msg.key.remoteJid, msg.message, {
+//         messageId: msg.key.id
+//     });
+// };
+// break
 case 'ringtone': {
 		if (!text) return replyTan(`Example : ${prefix + command} black rover`)
         let { ringtone } = require('./lib/scraper')
